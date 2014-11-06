7b00c1e30b80c5094887c7ecf8630f11fb1dd148
Jonathan Hoyt <hoyt@github.com>
2014-11-05 22:32:41 -0800

    Refactor Charge.find_for_account scope

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -2,13 +2,13 @@ class Charge < ActiveRecord::Base
   belongs_to :account
   has_many :states, :class_name => "ChargeState"
 
-  scope :all_for_account, ->(account) { where(:account => account) }
+  scope :all_for_account,
+    ->(account) { where(:account => account) }
+
+  scope :find_for_account,
+    ->(account, charge_id) { all_for_account(account).find(charge_id) }
 
   before_create :processing!
-
-  def self.find_for_account(account, charge_id)
-    all_for_account(account).find(charge_id)
-  end
 
   def readonly?
     persisted?
