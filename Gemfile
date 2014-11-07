669cd811bce22f29325e1988e8bba7c51451c7d9
Jonathan Hoyt <hoyt@github.com>
2014-11-06 16:01:42 -0800

    Add seeds

--- Gemfile
+++ Gemfile
@@ -9,4 +9,8 @@ gem "jquery-rails"
 gem "turbolinks"
 gem "jbuilder", "~> 2.0"
 gem "sdoc", "~> 0.4.0",          group: :doc
-gem "spring",        group: :development
+
+group :development do
+  gem "faker"
+  gem "spring"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+t = 2_000_000.seconds.ago
+
+100.times do
+  account = Account.create :name => Faker::Name.name, :created_at => t - 1.second
+  plan_amount_in_cents = [700, 1200, 2200, 2500].sample
+
+  10.times do
+    charge = Charge.create \
+      :account => account,
+      :amount_in_cents => plan_amount_in_cents,
+      :created_at => t
+
+    action = [nil, :processed!, :failed!].sample
+    charge.send(action) if action
+
+    t += 1.second
+  end
+end
